1e310d801a1176ca586ca19a92be86436d893fbe
Gary Leung <wtleung@hku.hk>
2025-03-03 03:50:03 +0000

    add exercise

---
+++ exercise.js
@@ -0,0 +1,4 @@
+// Exercise 1: Reverse a string
+export const reverseString = (str) => {
+    return str.split('').reverse().join('');
+}
---
+++ exercise.test.js
@@ -0,0 +1,8 @@
+import { reverseString } from './exercise.js';
+
+test('reverseString reverses the string', () => {
+    expect(reverseString('hello')).toBe('olleh');
+    // add more tests here
+    expect(reverseString('hello world')).toBe('dlrow olleh');
+});
+
